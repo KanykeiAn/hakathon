403944baba8a59f1778e730d8d6d5202fe82c364
Nuriza04 <aitbaevanuriza347@gmail.com>
2022-05-12 11:47:45 +0600

    footer and homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import Home from '../Components/home/Home';
+
+
+
+
 
 const HomePage = () => {
     return (
      <>
-        <img src="https://images.squarespace-cdn.com/content/v1/568aa781a976af7af9b0e2d3/1454016529370-OIFWN31B4HG3F9IUKDE2/stock-photo-82995507-french-macaroons-with-coffee-beans.jpg?format=1000w" className='back' alt="" />
+     <Home/>
+     
      </>
     );
 };
 
 export default HomePage;
+
+
+
+
+
+
+
+
+
+
